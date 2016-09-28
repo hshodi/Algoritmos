e103d2c904701fc738b75afb3665703b5a240497
Henrique Shodi Maeta <japa1996@hotmail.com>
2016-09-28 04:48:34 -0300

    Att

--- teste.c
+++ teste.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
+#include <time.h>
+#if !defined( Hello )
 
 void bubble(int a[], int size){
 	int i;
@@ -19,23 +18,27 @@ void bubble(int a[], int size){
 }
 
 int main(int argc, char const *argv[]){
-
+	clock_t begin = clock();
+	FILE *arq;
 	int *a;
-	a = malloc(10000 * sizeof(4));
+	a = malloc(10000000 * sizeof(4));
 	
-	int tam = 10000;
+	int tam = 10000000;
 
 	int j;
 	int i;
 	for(i = 0; i < tam; i++){
-		a[i] = rand() % 1000000;
+		a[i] = rand() % 1000000000;
 	}
 	bubble(a, tam);
+	arq = fopen("teste.txt", "w+");
 	for(j = 0; j < tam; j++){
-		printf("%d ", a[j]);
+		fprintf(arq, "%d\n", a[j]);
 	}
-	printf("\n");
-	
-
+	free(arq);
+	free(a);
+	clock_t end = clock();
+	double total = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("%.5lf", total);
 	return 0;
 }
