df9d51890aa101e45e33ea86358d55aac579e31d
Henrique Shodi Maeta <japa1996@hotmail.com>
2017-02-25 12:11:36 -0300

    Condicoes de x >= 0

--- Fortran/sqrtApproach.f90
+++ Fortran/sqrtApproach.f90
@@ -4,15 +4,28 @@ PROGRAM SQRT_APPROACH
     REAL(16) :: VALUE, X1, X2
 
     READ *, VALUE
+    
+    IF (VALUE.EQ.0) THEN
+        X1 = 0  
+        GOTO 200
+    END IF
 
+    IF (VALUE.LT.0) THEN
+        WRITE (*, *) 'NAO POSSUI SOLUCAO REAL'
+        GOTO 300
+    END IF
+    
     X1 = (VALUE + (VALUE / VALUE)) / 2
     
 100 X2 = (X1 + (VALUE / X1)) / 2
     IF (.NOT.(X1.EQ.X2)) THEN
         X1 = (X2 + (VALUE / X2)) / 2
-        GO TO 100
+        GOTO 100
     ELSE
-        WRITE (*, *) 'O RESULTADO APROXIMADO DA RAIZ E:', X1
+        GOTO 200
     END IF
 
-END PROGRAM SQRT_APPROACH
+200 WRITE (*, *) 'O RESULTADO APROXIMADO DA RAIZ E:', X1
+
+
+300 END PROGRAM SQRT_APPROACH
