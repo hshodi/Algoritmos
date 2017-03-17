f76b4b258434eaa4e8f73260b68ff8c9aeb8b591
Henrique Shodi Maeta <japa1996@hotmail.com>
2017-03-17 00:30:29 -0300

    Correcao do problema na funcao de insercaao iterativa

--- C/aulaED/arvore_binaria/arvore.c
+++ C/aulaED/arvore_binaria/arvore.c
@@ -30,11 +30,37 @@ void insere(Arvore **x, int val){
     printf("O elemento %d ja existe na arvore\n", val);
 }
 
-int find_value(Arvore **a, int val){
+void insere_iterative(Arvore **a, int val){
+
+    Arvore *first_position = *a;
+
+s2: if(*a != NULL){
+        if(val > (*a)->value){
+            *a = (*a)->direita;
+            goto s2;
+        }
+        if(val < (*a)->value){
+            *a = (*a)->esquerda;
+            goto s2;
+        }
+        if(val == (*a)->value){
+            printf("O elemento %d ja existe na arvore\n", val);
+            return;
+        }
+    }
+    Arvore *aux = (Arvore *)malloc(sizeof(Arvore));
+    aux->esquerda = aux->direita = NULL;
+    aux->value = val;
+    *a = aux;
+    printf("Elemento %d inserido com sucesso\n", (*a)->value);
+    *a = first_position;
+    return;
+}
+void find_value(Arvore **a, int val){
 
     if(*a == NULL){
-        printf("VALOR NAO ESTA NA ARVORE\n");
-        return 0;
+        printf("VALOR %d NAO ESTA NA ARVORE\n", (*a)->value);
+        return;
     }
     if(val > (*a)->value){
         find_value(&(*a)->direita, val);
@@ -45,12 +71,31 @@ int find_value(Arvore **a, int val){
         return;
     }
     if(val == (*a)->value){
-        printf("VALOR ENCONTRADO\n");
-        return 1; 
+        printf("VALOR %d ENCONTRADO\n", (*a)->value);
+        return; 
     }
 }
 
+int find_value_iterative(Arvore **a, int val){
 
+    s1: if(*a != NULL){
+        printf("VALUE: %d\n", (*a)->value);
+        if((*a)->value > val){
+            *a = (*a)->esquerda;
+            goto s1;
+        }
+        if((*a)->value < val){
+            *a = (*a)->direita;
+            goto s1;
+        }
+        if((*a)->value == val){
+            printf("VALOR %d ENCONTRADO\n", (*a)->value);
+            return 1;
+        }
+    }       
+    printf("VALOR %d NAO ESTA NA ARVORE\n", (*a)->value);
+    return 0;
+}
 
 void pre_order(Arvore **a){
 
@@ -86,12 +131,14 @@ int main(int argc, char* argv[]){
 
     Arvore *a = NULL;
     insere(&a, 5);
-    insere(&a, 5);
-    insere(&a, 10);
+    insere_iterative(&a, 5);
     insere(&a, 1);
+    insere_iterative(&a, 10);
+    in_order(&a);
     insere(&a, 0);
     insere(&a, 22);
-    find_value(&a, 31);
+    find_value(&a, 5);
+    find_value_iterative(&a, 21);
     free(a);
 
 
