80453101732fb73cdcecc6b15e554d6b7979c278
Henrique Shodi Maeta <japa1996@hotmail.com>
2017-12-20 16:25:25 -0200

    Fibonacci em kotlin

--- Kotlin/src/main/kotlin/Main.kt
+++ Kotlin/src/main/kotlin/Main.kt
@@ -2,4 +2,11 @@ package demo
 
 fun main(args: Array<String>) {
     println("kotlin!")
+    println(fib(5));
+}
+
+fun fib(n: Int): Int {
+    if (n < 2)
+        return n
+    return fib(n - 1) + fib(n - 2)
 }
